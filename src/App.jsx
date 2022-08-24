c90cbd01f2144e0c39e4e7255b496c3dd3d103da
robkarrr <93181892+robkarrr@users.noreply.github.com>
2022-08-24 11:40:09 +0200

    Added navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router-dom'
 import './assets/App.scss'
 import HomePage from './pages/HomePage'
 import NotFoundPage from './pages/NotFoundPage'
-
+import Navigation from './components/Nav'
 
 function App() {
   return (
     <div id="App">
+        <Navigation/>
+
         <Routes>
             <Route path="/" element={<HomePage/>}/>
             <Route path="*" element={<NotFoundPage/>}/>
