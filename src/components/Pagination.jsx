b9561b5eda667c98b9088ac9ad908f9620ca83f8
robkarrr <93181892+robkarrr@users.noreply.github.com>
2022-08-29 02:11:29 +0200

    Added pagination

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+
+const Pagination = ({page, totPages, onChangePage}) => {
+  return (
+      <div className="d-flex align-items-center justify-content-center">
+          
+          <Button 
+            variant='dark'
+            disabled={page -1 === 0}
+            onClick={() => onChangePage({page: Number(page) - 1}) }
+          >Previos</Button>
+
+          <span>{page}/{totPages}</span>
+
+          <Button 
+            variant='dark'
+            disabled={page >= totPages }
+            onClick={() => onChangePage({page: Number(page) + 1}) }
+          >Next</Button>
+      </div>
+  )
+}
+
+export default Pagination
--- src/pages/MovieByGenre.jsx
+++ src/pages/MovieByGenre.jsx
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container'
 import useMovieByGenre from '../hooks/useMovieByGenre'
 import {useParams, useSearchParams} from 'react-router-dom'
 import MovieCard from '../components/MovieCard'
+import Pagination from '../components/Pagination'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
@@ -11,29 +12,37 @@ const MovieByGenre = () => {
     const { id } = useParams()
     const [searchParams, setSearchParams] = useSearchParams({ page: 1 })
     const page = searchParams.get('page')
-    const {data, isSuccess, isError, error} = useMovieByGenre(id, page)
+    const {data, isSuccess, isError, isLoading, error} = useMovieByGenre(id, page)
 
 
 
-    return (
-     <Container className="py-3">
-            <div>
-                {isError && (
-                    <h1>Something went wrong</h1>
-                )}
-                <div>
-                    <h2>Genre Movies!</h2>
-                    <Row lg={3} md={4} sm={6}>
-                        {isSuccess && data.results.map((movie,id) => (
-                            <Col key={id}>
-                                <MovieCard movie={movie} id={id} />
-                            </Col>
-                        ))}
-                    </Row>
-                </div>
-            </div>
-        </Container>
-  )
+return (
+    <Container className="py-3">
+        <h1>Genre Movies</h1>
+        {isLoading && <h1>Loading movies</h1>}
+
+        {isError && error.message}
+
+        {isSuccess && (
+            <>
+                <Row>
+                    {data.results.map((movie,id) =>(
+                        <Col key={id} lg={3} md={4} sm={6}>
+                            <MovieCard movie={movie}/>
+                        </Col>
+                    ))}
+                </Row>
+
+                <Pagination
+                    page={page}
+                    totPages={data.total_pages}
+                    onChangePage={setSearchParams}
+                />
+            </>
+        )}
+
+    </Container>
+)
 }
 
 export default MovieByGenre
