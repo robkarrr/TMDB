ba5296d970e0659f2018b0dc8fc710474a95357f
robkarrr <93181892+robkarrr@users.noreply.github.com>
2022-08-29 18:32:04 +0200

    added adult=false in requests

--- src/services/tmbdAPI.js
+++ src/services/tmbdAPI.js
@@ -3,22 +3,23 @@ import axios from 'axios'
 axios.defaults.baseURL = 'https://api.themoviedb.org/3'
 
 const api_key = import.meta.env.VITE_TMDB_API_KEY
+const adult_false = `&include_adult=false`
 
 
 const getPopularMovies = async () => {
-    const res = await axios.get(`/movie/popular?api_key=${api_key}&language=en-US&page=1`)
+    const res = await axios.get(`/movie/popular?api_key=${api_key}&language=en-US&page=1${adult_false}`)
 
     return res.data
 }
 
 const getLatestMovies = async () => {
-    const res = await axios.get(`movie/now_playing?api_key=${api_key}&language=en-US&page=1`)
+    const res = await axios.get(`movie/now_playing?api_key=${api_key}&language=en-US&page=1${adult_false}`)
 
     return res.data
 }
 
 const getTopRatedMovies = async () => {
-    const res = await axios.get(`movie/top_rated?api_key=${api_key}&language=en-US&page=1`)
+    const res = await axios.get(`movie/top_rated?api_key=${api_key}&language=en-US&page=1${adult_false}`)
 
     return res.data
 }
@@ -30,7 +31,7 @@ const getGenres = async () => {
 }
 
 const getMovieByGenre = async (genre, page) => {
-    const res = await axios.get(`/discover/movie?api_key=${api_key}&with_genres=${genre}&page=${page}`)
+    const res = await axios.get(`/discover/movie?api_key=${api_key}&with_genres=${genre}&page=${page}${adult_false}`)
 
     return res.data
 }
