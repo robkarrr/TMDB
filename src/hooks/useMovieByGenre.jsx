bc175ec5153a56f6590f3e12e14c0e31f9aca259
robkarrr <93181892+robkarrr@users.noreply.github.com>
2022-08-29 02:17:25 +0200

    Added keepPreviousData

--- src/hooks/useMovieByGenre.jsx
+++ src/hooks/useMovieByGenre.jsx
@@ -2,7 +2,7 @@ import { useQuery } from 'react-query'
 import tmdbAPI from '../services/tmbdAPI'
 
 const useMovieByGenre = (id,page) => {
-    return useQuery(['movies-by-genre', {id, page}], () => tmdbAPI.getMovieByGenre(id,page))
+    return useQuery(['movies-by-genre', {id, page}], () => tmdbAPI.getMovieByGenre(id,page), {keepPreviousData:true})
 }
 
 export default useMovieByGenre
